72965360174705f384166146fa21a1d3decfafc6
Linda Sandaker <linda.m.sandaker@gmail.com>
2021-12-14 01:31:16 +0100

    So much trouble getting grid to work, worked all weekend on that, had to give it up for now and fix other content on landing page...

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
+const
+    e = document.querySelector(".primary-navigation-menu"),
+    n = document.querySelectorAll(".menu-item"),
+    o = document.querySelector(".primary-navigation-hamburgerbtn"),
+    t = document.querySelector(".close-navigation-icon"),
+    c = document.querySelector(".open-navigation-icon");
 
-
-const primaryNavigationMenu = document.querySelector(".primary-navigation-menu");
-const menuItems = document.querySelectorAll(".menu-item");
-const primNavSubmenu = document.querySelector(".prim-nav-submenu");
-const primaryNavigationHamburgerbtn = document.querySelector(".primary-navigation-hamburgerbtn");
-const closeNavigationIcon = document.querySelector(".close-navigation-icon");
-const openNavigationIcon = document.querySelector(".open-navigation-icon");
-
-function toggleMenu() {
-    if (primaryNavigationMenu.classList.contains("showMenu")) {
-        primaryNavigationMenu.classList.remove("showMenu");
-        closeNavigationIcon.style.display = "none";
-        openNavigationIcon.style.display = "block";
-    } else {
-        primaryNavigationMenu.classList.add("showMenu");
-        closeNavigationIcon.style.display = "block";
-        openNavigationIcon.style.display = "none";
-    }
+function i() {
+    e.classList.contains("showMenu") ?
+        (e.classList.remove("showMenu"),
+            t.style.display = "none",
+            c.style.display = "block") :
+        (e.classList.add("showMenu"),
+            t.style.display = "block",
+            c.style.display = "none");
 }
 
-primaryNavigationHamburgerbtn.addEventListener("click", toggleMenu);
+o.addEventListener("click", i),
 
-
-
-menuItems.forEach(
-    function (menuItem) {
-        menuItem.addEventListener("click", toggleMenu);
-        menuItem.addEventListener("hover", toggleMenu);
-    }
-);
+    n.forEach((function (e) {
+        e.addEventListener("click", i);
+    }));
